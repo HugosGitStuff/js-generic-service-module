70dd49d749dc61a6707a361f2bfb906fd94729bb
Hugo Ramalho <hugotransport@icloud.com>
2025-12-09 10:03:22 +0000

    added try catch

--- service.js
+++ service.js
@@ -9,8 +9,7 @@ class Service {
 
   // add method
   add(item) {
-    // push item into items array
-    this.items.push(item);
+    this.items.push(item);  // push item into items array
   }
 
   // list method
@@ -26,8 +25,7 @@ class Service {
   // update existing item
   // newData: object with properties to update
   update(id, newData) {
-    // find item to update
-    const item = this.get(id);
+    const item = this.get(id) // find item to update
     
     // if exists, update properties with newData
     if (item) {
@@ -46,6 +44,8 @@ class Service {
   }
   
 }
+
+try {
 
 // new service
 const userService = new Service("UserService");
@@ -76,35 +76,40 @@ userService.add({
 /* 
 //test add method
 console.log(userService.items);
+*/
 
 //test list method
-console.log(userService.list());
+console.log("\n List of items: ",userService.list());
 
 // test get method
-console.log(userService.get(2));
+console.log("\n",userService.get(2));
 console.log(userService.get(3)); 
 
 // test update method 
 // before update
-console.log("Before update: ", userService.get(1));
+console.log("\nBefore update: ", userService.get(1));
 // update user with id 1... add a new property "school"
 userService.update(1, { school: "CFA" });
 // after update
-console.log("\nAfter update: ", userService.get(1));
+console.log("After update: ", userService.get(1));
 
 
 // test remove method
 // before remove
-console.log("Before remove: ", userService.list());
+console.log("\nBefore remove: ", userService.list());
 // remove user with id 2
 userService.remove(2);
 // after remove
-console.log("\nAfter remove: ", userService.list());
-*/
+console.log("After remove: ", userService.list());
+
 
 // before destroy
-console.log("Before destroy: ", userService.list());
+console.log("\nBefore destroy: ", userService.list());
 // destroy the service
 userService.destroy();
 // After destroy
-console.log("\nAfter destroy: ", userService.list());
+console.log("After destroy: ", userService.list());
+
+} catch (error) {
+  console.log("Error caught:", error.message);
+}
